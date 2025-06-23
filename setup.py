bb455856b2a753e3173c6ee1d1d84c6d73966fe3
Kiplangat Korir <korirkiplangat22@gmail.com>
2025-06-23 16:17:18 +0300

    Updated Setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="graphfusionai",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "networkx",
+        "matplotlib"
+    ],
+    entry_points={
+        "console_scripts": [
+            "graphfusionai=graphfusionai.cli:main",
+        ],
+    },
+)
